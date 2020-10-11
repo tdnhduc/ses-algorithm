51f287d9c6475051dc72ff1db98744d4c8dc48fc
DucNguyen <nguoihanhtinh123@gmail.com>
2020-10-11 23:07:12 +0700

    init project

---
+++ ses/src/main/java/com/hcmus/fit/network/distributedsystem/Runner.java
@@ -0,0 +1,11 @@
+package com.hcmus.fit.network.distributedsystem;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Runner {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Runner.class);
+    public static void main(String args[]){
+        LOGGER.info("Hello world");
+    }
+}
